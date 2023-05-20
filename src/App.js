87c9fe0e926376fe0756af14e6807a653427e265
psarasua <74331843+pablinuxx@users.noreply.github.com>
2023-05-20 20:46:18 -0300

    diseño de login y panel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Panel from "./components/Panel/Panel";
 
 export default function App() {
-  return <div>Caca</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" Component={Login}></Route>
+        <Route path="/" Component={Panel}></Route>
+      </Routes>
+    </>
+  );
 }
