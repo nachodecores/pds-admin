23830cdf772785538192121deede38cd6d4fdf76
Oco <92460234+nachodecores@users.noreply.github.com>
2023-05-24 16:18:47 -0300

    mas de input de lotes (herds)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Panel from "./components/Panel/Panel";
+import Herd from "./components/Forms/Herd";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/login" Component={Login}></Route>
         <Route path="/" Component={Panel}></Route>
+        <Route path="/lote" Component={Herd}></Route>
       </Routes>
     </>
   );
